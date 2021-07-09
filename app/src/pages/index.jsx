7c15df1eedfec2531e7c86cbc44f88968a734994
Joaovitorufu <joaovitorufu@gmail.com>
2021-07-09 09:40:40 -0300

    estrutura basica

--- app/src/pages/index.jsx
+++ app/src/pages/index.jsx
@@ -1,9 +1,12 @@
 import '../styles/Home.module.css'
 
+import {Footer} from '../components/Footer'
+
 export default function Home() {
   return (
     <div >
         <h1>Pagina home</h1>
+        <Footer/>
     </div>
   )
 }
